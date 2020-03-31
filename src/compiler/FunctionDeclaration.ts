19b340f1e0ed57badcc52c4746c508f51dacf3b6
shortercode <iainshorter@gmail.com>
2020-03-31 20:45:34 +0100

    add list of locals to FunctionDeclaration

--- src/compiler/FunctionDeclaration.ts
+++ src/compiler/FunctionDeclaration.ts
@@ -3,6 +3,8 @@ import { AtiumType } from "./AtiumType.js";
 export class FunctionDeclaration {
     type: AtiumType
     parameters: Array<{ name: string, type: AtiumType}>
+    locals: Array<AtiumType>
+
     constructor (type: AtiumType, parameters: Array<{ name: string, type: AtiumType }>) {
         this.type = type;
         this.parameters = parameters;

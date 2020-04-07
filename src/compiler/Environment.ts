9f6f34513b3f84e3fae9a25723282cea10f668bb
shortercode <iainshorter@gmail.com>
2020-04-07 15:43:27 +0100

    add the ability to create a "hidden" var for temp values

--- src/compiler/Environment.ts
+++ src/compiler/Environment.ts
@@ -30,13 +30,22 @@ export class Environment {
 		}
 
     declare (name: string, type: AtiumType): Variable {
-      	if (this.current_frame.has(name))
-      		  throw new Error(`Variable ${name} already exists in the current scope`);
-      	const variable = new Variable(type, name, this.id_counter++);
-				this.current_frame.set(name, variable);
-				this.variables.push(variable);
-				return variable;
-    }
+			if (this.current_frame.has(name))
+					throw new Error(`Variable ${name} already exists in the current scope`);
+			const variable = this.create_variable(name, type);
+			this.current_frame.set(name, variable);
+			return variable;
+		}
+		
+		declare_hidden (name: string, type: AtiumType): Variable {
+			return this.create_variable(name, type);
+		}
+
+		private create_variable (name: string, type: AtiumType): Variable {
+			const variable = new Variable(type, name, this.id_counter++);
+			this.variables.push(variable);
+			return variable;
+		} 
 
     get_variable (name: string): Variable | null {
 				for (const frame of this.frame_stack) {

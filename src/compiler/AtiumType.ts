c7459d54aa961905b504d62860e374a8a0e131fc
shortercode <iainshorter@gmail.com>
2020-04-12 16:40:39 +0100

    Replace the string union used for types with a class

--- src/compiler/AtiumType.ts
+++ src/compiler/AtiumType.ts
@@ -1,39 +1,84 @@
-type PrimativeTypes = "f32" | "f64" | "i32" | "i64" | "void";
+export enum PrimativeTypes {
+	f32,
+	f64,
+	i32,
+	i64,
+	void,
+	boolean
+} 
 
-export type AtiumType = PrimativeTypes | "boolean";
+const integer_types = new Set([
+	PrimativeTypes.i32,
+	PrimativeTypes.i64
+]);
+const float_types = new Set([
+	PrimativeTypes.f32,
+	PrimativeTypes.f64
+]);
+const numeric_types = new Set([
+	...integer_types,
+	...float_types
+]);
 
-export function is_numeric (type: AtiumType) {
-	switch (type) {
+export type AtiumType = PrimativeAtiumType;
+
+class PrimativeAtiumType {
+	private readonly type: PrimativeTypes
+	readonly name: string
+
+	constructor (type: PrimativeTypes, name: string) {
+		this.type = type;
+		this.name = name;
+	}
+
+	equals (other: AtiumType): boolean {
+		return this.type === other.type;
+	}
+
+	is_numeric (): boolean {
+		return numeric_types.has(this.type);
+	}
+
+	is_integer (): boolean {
+		return integer_types.has(this.type);
+	}
+
+	is_float (): boolean {
+		return float_types.has(this.type);
+	}
+
+	wasm_type (): PrimativeTypes {
+		return this.type;
+	}
+
+	is_boolean (): boolean {
+		return this.type === PrimativeTypes.boolean;
+	}
+
+	is_void (): boolean {
+		return this.type === PrimativeTypes.void;
+	}
+
+	is_exportable (): boolean {
+		return this.type !== PrimativeTypes.i64;
+	}	
+}
+
+function validate_primative_type (str: string): PrimativeTypes {
+	switch (str) {
 		case "f32":
 		case "f64":
 		case "i32":
 		case "i64":
-		return true;
-		default:
-		return false;
-	}
-}
-
-export function is_integer (type: AtiumType) {
-	return type === "i32" || type === "i64";
-}
-
-export function validate_atium_type(type: string): AtiumType {
-	// TODO this will need changing when we add objects
-	switch (type) {
-		case "boolean":
 		case "void":
-		case "f32":
-		case "f64":
-		case "i32":
-		case "i64":
-		return type;
+		case "boolean":
+		return PrimativeTypes[str];
 		default:
-		throw new Error(`Illegal type ${type}`);
+		throw new Error(`Cannot parse type "${str}" as primative type`);
 	}
 }
 
-export function compare_atium_types(a: AtiumType, b: AtiumType): boolean {
-	// TODO this will need changing when we add objects
-	return a === b;
+export function parse_type (str: string) {
+	const type_name = validate_primative_type(str);
+	return new PrimativeAtiumType(type_name, str);
 }

8e99d9f4de561cfd2b5da09ed1edfc15b319bd01
shortercode <iainshorter@gmail.com>
2020-04-22 17:21:05 +0100

    Update StructDeclaration.ts
    generate struct type with declaration

--- src/compiler/StructDeclaration.ts
+++ src/compiler/StructDeclaration.ts
@@ -1,11 +1,20 @@
-import { AtiumType } from "./AtiumType";
+import { AtiumType, StructAtiumType } from "./AtiumType";
 
 export class StructDeclaration {
 	name: string
+	type: StructAtiumType
+	size: number
 	fields: Map<string, AtiumType>
 	
 	constructor (name: string, fields: Map<string, AtiumType>) {
 		this.name = name;
 		this.fields = fields;
+		this.type = new StructAtiumType(fields, name);
+
+		let size = 0;
+		for (const type of fields.values()) {
+			size += type.size;
+		}
+		this.size = size;
 	}
 }

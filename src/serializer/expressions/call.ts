437a9d10bf69f1f4f88f2ade8699c24aea339d55
shortercode <iainshorter@gmail.com>
2020-05-07 11:29:32 +0100

    tidy: call expression was using throw instead of compiler_assert

--- src/serializer/expressions/call.ts
+++ src/serializer/expressions/call.ts
@@ -1,18 +1,17 @@
 import { WASTCallNode, WASTExpressionNode } from "../../WASTNode";
 import { Opcode } from "../OpCode";
 import { FunctionContext } from "../FunctionContext";
+import { compiler_assert } from "../../compiler/error";
 
 type WriteExpression = (ctx: FunctionContext, node: WASTExpressionNode) => void;
 
 export function write_call_expression(ctx: FunctionContext, node: WASTExpressionNode, write_expression: WriteExpression) {
 	const call_node = node as WASTCallNode;
 	
-	const function_id = ctx.function_lookup.get(call_node.id);
+	const function_id = ctx.function_lookup.get(call_node.id)!;
 	
-	if (typeof function_id !== "number") {
-		throw new Error(`Cannot find function ${call_node.name}`);
-	}
-	
+	compiler_assert(typeof function_id !== "number", node.source, `Cannot find function ${call_node.name}`);
+
 	for (const arg of call_node.arguments) {
 		write_expression(ctx, arg);
 		ctx.consume_value(arg.value_type.wasm_type(), node.source);

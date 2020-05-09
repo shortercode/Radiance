b9af19b00752a874960753546f4c5531aa4d7d5c
shortercode <iainshorter@gmail.com>
2020-05-09 13:59:16 +0100

    Add warning to trap regarding stack_depth management

--- src/serializer/expressions/trap.ts
+++ src/serializer/expressions/trap.ts
@@ -4,5 +4,6 @@ import { Opcode } from "../OpCode";
 
 export function write_trap_expression(ctx: FunctionContext, node: WASTExpressionNode) {
 	const trap_node = node as WASTTrapNode;
+	// WARN this does not reset the stack_depth variable
 	ctx.writer.writeUint8(Opcode.unreachable);
 }

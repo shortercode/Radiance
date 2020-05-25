bef1fde60a5b58ecf3fcd868d381abd3509914ba
shortercode <iainshorter@gmail.com>
2020-05-25 10:33:39 +0100

    chore: whitespace correction in pratt

--- src/pratt/Iterator.ts
+++ src/pratt/Iterator.ts
@@ -1,51 +1,52 @@
 export default class ControlledIterator<T> {
-    private _iterator: Iterator<T>
-    private _previous: IteratorResult<T> | null
-    private _current: IteratorResult<T>
-    private _next: IteratorResult<T>
-    private _future: IteratorResult<T> | null
-
-    constructor (iterable: Iterable<T>) {
-        const itr = iterable[Symbol.iterator]();
-        this._iterator = itr;
-        this._previous = null;
-        this._current = itr.next();
-        this._next = itr.next();
-        this._future = null;
-    }
-    next (): IteratorResult<T> {
-        this._previous = this._current;
-        this._current = this._next;
-        this._next = this._future || this._iterator.next();
-        this._future = null;
-
-        return this._previous;
-    }
-    incomplete (): boolean {
-        return !this._current.done;
-    }
-    consume (): T | null {
-        return this.next().value;
-    }
-    back () {
-        if (this._previous === null)
-            throw new Error("Exceeded step back buffer length");
-
-        this._future = this._next;
-        this._next = this._current;
-        this._current = this._previous;
-        this._previous = null;
-    }
-    previous (): T | null {
-        return this._previous && this._previous.value;
-    }
-    peek (): T | null {
-        return this._current.value;
-    }
-    peekNext (): T | null {
-        return this._next.value;
-    }
-    [Symbol.iterator] () {
-        return this;
-    }
+	private _iterator: Iterator<T>
+	private _previous: IteratorResult<T> | null
+	private _current: IteratorResult<T>
+	private _next: IteratorResult<T>
+	private _future: IteratorResult<T> | null
+	
+	constructor (iterable: Iterable<T>) {
+		const itr = iterable[Symbol.iterator]();
+		this._iterator = itr;
+		this._previous = null;
+		this._current = itr.next();
+		this._next = itr.next();
+		this._future = null;
+	}
+	next (): IteratorResult<T> {
+		this._previous = this._current;
+		this._current = this._next;
+		this._next = this._future || this._iterator.next();
+		this._future = null;
+		
+		return this._previous;
+	}
+	incomplete (): boolean {
+		return !this._current.done;
+	}
+	consume (): T | null {
+		return this.next().value;
+	}
+	back () {
+		if (this._previous === null) {
+			throw new Error("Exceeded step back buffer length");
+		}
+		
+		this._future = this._next;
+		this._next = this._current;
+		this._current = this._previous;
+		this._previous = null;
+	}
+	previous (): T | null {
+		return this._previous && this._previous.value;
+	}
+	peek (): T | null {
+		return this._current.value;
+	}
+	peekNext (): T | null {
+		return this._next.value;
+	}
+	[Symbol.iterator] () {
+		return this;
+	}
 }
--- src/pratt/Trie.ts
+++ src/pratt/Trie.ts
@@ -1,57 +1,58 @@
 export default class Trie extends Map {
-    value: string | null
-
-    constructor (itr: Iterable<string> | null) {
-        super();
-        this.value = null;
-        if (itr !== null) {
-            for (const sym of itr) 
-                this.add(sym);
-        }
-    }
-    add (key: string, value: string = key) {
-        let node = this;
-
-        for (const char of key) {
-            let child = node.get(char);
-            if (!child) {
-                child = new Trie(null);
-                node.set(char, child);
-            }
-            node = child;
-        }
-
-        node.value = value;
-    }
-    remove (key: string) {
-        if (key.length === 0)
-            return;
-
-        const path: Array<[Trie, string]> = [];
-        let node = this;
-
-        for (const char of key) {
-            const child = node.get(char);
-            if (!child) return; // branch doesn't exist - exit
-            path.push([node, char]);
-            node = child;
-        }
-
-        // remove leaf
-        node.value = null;
-
-        // no children, remove this branch
-        if (node.size === 0) {
-            const [parent, char] = path.pop()!;
-            parent.delete(char);
-        }
-    }
-    find(key: string): string|null {
-        let node = this;
-        for (const char of key) {
-            node = node.get(char);
-            if (!node) return null;
-        }
-        return node.value;
-    }
+	value: string | null
+	
+	constructor (itr: Iterable<string> | null) {
+		super();
+		this.value = null;
+		if (itr !== null) {
+			for (const sym of itr) {
+				this.add(sym);
+			}
+		}
+	}
+	add (key: string, value: string = key) {
+		let node = this;
+		
+		for (const char of key) {
+			let child = node.get(char);
+			if (!child) {
+				child = new Trie(null);
+				node.set(char, child);
+			}
+			node = child;
+		}
+		
+		node.value = value;
+	}
+	remove (key: string) {
+		if (key.length === 0)
+		return;
+		
+		const path: Array<[Trie, string]> = [];
+		let node = this;
+		
+		for (const char of key) {
+			const child = node.get(char);
+			if (!child) return; // branch doesn't exist - exit
+			path.push([node, char]);
+			node = child;
+		}
+		
+		// remove leaf
+		node.value = null;
+		
+		// no children, remove this branch
+		if (node.size === 0) {
+			const [parent, char] = path.pop()!;
+			parent.delete(char);
+		}
+	}
+	find(key: string): string|null {
+		let node = this;
+		for (const char of key) {
+			node = node.get(char);
+			if (!node) return null;
+		}
+		return node.value;
+	}
 }

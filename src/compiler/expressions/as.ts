86dbf350c4daea81bf456e313e18800e250820d4
shortercode <iainshorter@gmail.com>
2020-05-30 16:40:47 +0100

    add: update the as expression to support the casting of tuples
    Casting tuples is much more complicated than primatives, but is doable provided the elements are composed only of primatives and tuples ( also of tuples/primatives ).
    
    This implements the change. It additionally allows to convert to a tuple with less elements than the source, discarding part of the tuple.

--- src/compiler/expressions/as.ts
+++ src/compiler/expressions/as.ts
@@ -1,9 +1,10 @@
 import { AST, Compiler } from "../core";
 import { TypePattern } from "../../parser/index";
-import { WASTExpressionNode, WASTConvertToFloat, WASTConvertToInt, Ref } from "../../WASTNode";
-import { parse_type } from "../LangType";
+import { WASTExpressionNode, WASTConvertToFloat, WASTConvertToInt, Ref, WASTLoadNode, WASTNodeList } from "../../WASTNode";
+import { parse_type, TupleLangType, LangType } from "../LangType";
 import { type_assert, type_error } from "../error";
 import { wrap_boolean_cast } from "./boolean";
+import { create_object } from "./object";
 
 function read_node_data (node: AST) {
 	return node.data as {
@@ -13,30 +14,58 @@ function read_node_data (node: AST) {
 }
 
 export function visit_as_expression (compiler: Compiler, node: AST): WASTExpressionNode {
-	const value = read_node_data(node);
+	const data = read_node_data(node);
 	const ref = Ref.from_node(node);
 
-	const target_type = parse_type(value.type, compiler.ctx);
-	const new_value = compiler.visit_expression(value.expr, target_type);
+	const target_type = parse_type(data.type, compiler.ctx);
+	const value = compiler.visit_expression(data.expr, target_type);
 
-	if (target_type.equals(new_value.value_type)) {
-		return new_value;
+	return cast_expression(compiler, ref, value, target_type);
+}
+
+function cast_expression (compiler: Compiler, ref: Ref, value: WASTExpressionNode, output_type: LangType): WASTExpressionNode {
+	if (output_type.equals(value.value_type)) {
+		return value;
 	}
 
-	if (target_type.is_float()) {
-		type_assert(new_value.value_type.is_numeric(), node, `Unable to cast non-numeric type ${new_value.value_type.name} to ${target_type.name}`);
-
-		return new WASTConvertToFloat(ref, target_type, new_value);
+	if (output_type.as_tuple() !== null && value.value_type.as_tuple !== null) {
+		return visit_as_tuple_expression (compiler, ref, value, value.value_type.as_tuple()!, output_type.as_tuple()!);
 	}
-	else if (target_type.is_integer()) {
-		type_assert(new_value.value_type.is_numeric(), node, `Unable to cast non-numeric type ${new_value.value_type.name} to ${target_type.name}`);
+	else if (output_type.is_float()) {
+		type_assert(value.value_type.is_numeric(), ref, `Unable to cast non-numeric type ${value.value_type.name} to ${output_type.name}`);
 
-		return new WASTConvertToInt(ref, target_type, new_value);
+		return new WASTConvertToFloat(ref, output_type, value);
 	}
-	else if (target_type.is_boolean()) {
-		return wrap_boolean_cast(ref, new_value);
+	else if (output_type.is_integer()) {
+		type_assert(value.value_type.is_numeric(), ref, `Unable to cast non-numeric type ${value.value_type.name} to ${output_type.name}`);
+
+		return new WASTConvertToInt(ref, output_type, value);
+	}
+	else if (output_type.is_boolean()) {
+		return wrap_boolean_cast(ref, value);
 	}
 	else {
-		type_error(node, `Unable to cast to type ${target_type.name}`);
+		type_error(ref, `Unable to cast ${value.value_type.name} to type ${output_type.name}`);
 	}
+}
+
+function visit_as_tuple_expression (compiler: Compiler, ref: Ref, expr: WASTExpressionNode, input_type: TupleLangType, output_type: TupleLangType): WASTExpressionNode {
+	type_assert(input_type.types.length >= output_type.types.length, ref, `Cannot cast to a tuple with more elements than the input type`);
+
+	// TODO remove this
+	if (input_type.types.length === 0) {
+		return new WASTNodeList(ref); // this is equivilent to returning a void value, without actually returning one ( as void is the abscense of a value )
+	}
+
+	const values: Array<WASTExpressionNode> = [];
+
+	for (const [index, { type, offset }] of input_type.types.entries()) {
+		const read_value = new WASTLoadNode(ref, type, expr, offset);
+		const sub_type = output_type.types[index].type;
+		const value = cast_expression(compiler, ref, read_value, sub_type);
+
+		values.push(value);
+	}
+
+	return create_object(compiler, ref, output_type, values);
 }

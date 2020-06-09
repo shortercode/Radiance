82ca0e314c2643401035a1e9be0f5c76c230786c
shortercode <iainshorter@gmail.com>
2020-06-09 18:18:59 +0100

    fix: invalid expression value for conditionals without "else" clause
    The value of conditional expressions defaulted to the value of the "then" clause, then became "void" if the "else" clause had a different type. This mistake meant that conditions without an else clause appeared to have a value, but they could have been void.
    
    It now defaults to "void" and takes on a value if the 2 clauses are present and match.

--- src/compiler/expressions/if.ts
+++ src/compiler/expressions/if.ts
@@ -1,6 +1,6 @@
 import { Compiler, AST, TypeHint } from "../core";
 import { WASTExpressionNode, WASTNodeList, WASTConditionalNode, Ref } from "../../WASTNode";
-import { BOOL_TYPE, VOID_TYPE } from "../LangType";
+import { BOOL_TYPE, VOID_TYPE, LangType } from "../LangType";
 import { ensure_expression_emits_boolean } from "./boolean";
 
 function read_node_data (node: AST) {
@@ -19,12 +19,12 @@ export function visit_if_expression (compiler: Compiler, node: AST, type_hint: T
 	condition = ensure_expression_emits_boolean(condition);
 	
 	const then_branch = compiler.visit_expression(data.thenBranch, type_hint) as WASTNodeList;
-	let value_type = then_branch.value_type;
-
+	let value_type: LangType = VOID_TYPE;
+	
 	if (data.elseBranch !== null) {
 		const else_branch = compiler.visit_expression(data.elseBranch, type_hint) as WASTNodeList;
-		if (else_branch.value_type.equals(value_type) === false) {
-			value_type = VOID_TYPE;
+		if (else_branch.value_type.equals(then_branch.value_type)) {
+			value_type = then_branch.value_type;;
 		}
 		return new WASTConditionalNode(ref, value_type, condition, then_branch, else_branch);
 	}

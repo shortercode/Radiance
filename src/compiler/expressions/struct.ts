3b4fce43b022a1cc78ad396644c76e94598fd39c
shortercode <iainshorter@gmail.com>
2020-07-12 20:28:09 +0100

    ADD struct compiler now differentiates between structs and their templates

--- src/compiler/expressions/struct.ts
+++ src/compiler/expressions/struct.ts
@@ -7,7 +7,8 @@ import { Ref } from "../../WASTNode";
 function read_node_data (node: AST) {
 	return node.data as {
 		name: string,
-		fields: Map<string, TypePattern>
+		fields: Map<string, TypePattern>,
+		generics: string[]
 	};
 }
 
@@ -17,12 +18,23 @@ export function hoist_struct_declaration(compiler: Compiler, node: AST) {
 
 	syntax_assert(data.fields.size > 0, node, `Unable to declare struct ${data.name} as it has no fields. Structs must have at least 1 field`);
 	
-	const fields = new Map;
+	if (data.generics.length > 0) {
+		const fields = new Map;
 
-	for (const [name, pattern] of data.fields) {
-		const type = parse_type(pattern, ctx);
-		fields.set(name, type);
+		for (const [name, pattern] of data.fields) {
+			fields.set(name, pattern);
+		}
+	
+		ctx.declare_struct_template(Ref.from_node(node), data.name, fields, data.generics);
 	}
+	else {
+		const fields = new Map;
 
-	ctx.declare_struct(Ref.from_node(node), data.name, fields);
+		for (const [name, pattern] of data.fields) {
+			const type = parse_type(pattern, ctx);
+			fields.set(name, type);
+		}
+	
+		ctx.declare_struct(Ref.from_node(node), data.name, fields);
+	}
 }
